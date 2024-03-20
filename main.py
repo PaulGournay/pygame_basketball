695cd187af36730ec6d3d260424c6c1c59744b6c
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2024-03-20 23:16:28 +0100

    implementation of gravity and bounce

--- main.py
+++ main.py
@@ -19,26 +19,26 @@ arrow_rect = arrow_surf.get_rect(midbottom=(470, 400))
 player_surf = pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player.png').convert_alpha(), (70, 180)), True, False)
 player_rect = player_surf.get_rect(midbottom=(400, 480))
 ball_surf = pygame.transform.scale(pygame.image.load('assets/basketball ball.png').convert_alpha(), (80, 80))
-ball_rect = ball_surf.get_rect(midbottom=(400, 480))
+ball_rect = ball_surf.get_rect(center=(400, 480))
 hopper_surf = pygame.transform.scale(pygame.image.load('assets/hooooooop.png').convert_alpha(), (180, 300))
 hopper_rect = hopper_surf.get_rect(midbottom=(720, 480))
 
 # all ball values(parameters):
 x_ini = player_rect.x+30
 y_ini = player_rect.y+50
-x_val=x_ini
-y_val=y_ini
-x_pre= x_ini
-y_pre=y_ini
+x_val = x_ini
+y_val = y_ini
+x_pre = x_ini-30
+y_pre = y_ini
 mass = 100
-time=0
-time2=0
+time = 0
+time2 = 0
 retention = 0.95
 gravity = 0.5
 shoot = False
 angle = 0.0
 speed = 10.0
-trajectory =False
+trajectory = False
 
 
 while running == True:
@@ -70,9 +70,9 @@ while running == True:
                 ball_Sound.play()
                 time = 0  # Reset time after shooting
 
-    for time2 in range(0,30,5):
-        x_pre = (x_ini + math.cos(math.radians(angle)) * speed * time2)
-        y_pre = (y_ini - (math.sin(math.radians(angle)) * speed * time2) + 0.5 * gravity * time2 ** 2)
+    for time2 in range(0,60,3):
+        x_pre = (x_ini+40 + math.cos(math.radians(angle)) * speed * time2)
+        y_pre = (y_ini+40 - (math.sin(math.radians(angle)) * speed * time2) + 0.5 * gravity * time2 ** 2)
         pygame.draw.circle(back_ground_surf,'white',(x_pre,y_pre),5)
 
 
@@ -82,7 +82,7 @@ while running == True:
         x_val = (x_ini + math.cos(math.radians(angle)) * speed * time)
         y_val = (y_ini - (math.sin(math.radians(angle)) * speed * time) + 0.5 * gravity * time ** 2)
 
-        if y_val + ball_surf.get_height() >= back_ground_surf.get_height() + 30:
+        if y_val + ball_surf.get_height() >= back_ground_surf.get_height()+10:
             # Implement bounce
             y_val = back_ground_surf.get_height() - ball_surf.get_height()
             speed *= retention  # Reduce speed due to bounce
@@ -109,7 +109,6 @@ while running == True:
     screen.blit(back_ground_surf, back_ground_rect)
     screen.blit(player_surf, player_rect)
     screen.blit(hopper_surf, hopper_rect)
-    screen.blit(arrow_surf, arrow_rect)
     screen.blit(ball_surf, (x_val, y_val))
     pygame.draw.rect(screen, 'Blue', player_rect, 5)
     pygame.draw.rect(screen, 'Green', hopper_rect, 5)
