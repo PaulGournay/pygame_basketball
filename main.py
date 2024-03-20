aa3693cce2a024dfaed0a8d438d04c9f997e8ccc
sheepfd <vrk.karakozian@gmail.com>
2024-03-20 16:23:36 +0100

    new

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ game_active = True
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((950, 600))
 positions = [(200, 480), (250, 480), (300, 480), (350, 480), (400, 480)]
+back_ground_surf1 = pygame.transform.scale(pygame.image.load('assets/basketball court.png').convert_alpha(), (950, 600))
 back_ground_surf = pygame.transform.scale(pygame.image.load('assets/basketball court.png').convert_alpha(), (950, 600))
 back_ground_rect = back_ground_surf.get_rect(topleft=(0, 0))
 arrow_surf = pygame.image.load('assets/arrow.png').convert_alpha()
@@ -31,11 +32,14 @@ x_pre= x_ini
 y_pre=y_ini
 mass = 100
 time=0
+time2=0
 retention = 0.95
 gravity = 0.5
 shoot = False
 angle = 0.0
 speed = 10.0
+trajectory =False
+
 
 while running == True:
     for event in pygame.event.get():
@@ -46,25 +50,32 @@ while running == True:
         elif event.type == pygame.KEYDOWN and not shoot:
             if event.key == pygame.K_UP:
                 angle += 10
+                trajectory = True
                 print(angle)
             elif event.key == pygame.K_DOWN:
                 angle -= 10
+                trajectory = True
                 print(angle)
             elif event.key == pygame.K_RIGHT:
                 speed += 1
+                trajectory = True
                 print(speed)
             elif event.key == pygame.K_LEFT and speed >= 10:
                 speed -= 1
+                trajectory = True
                 print(speed)
             elif event.key == pygame.K_SPACE:
                 shoot = True
                 ball_Sound = mixer.Sound('assets/throw_sound.wav')
                 ball_Sound.play()
                 time = 0  # Reset time after shooting
-    """for time in range(0,30,5):
-        x_pre = (x_ini + math.cos(math.radians(angle)) * speed * time)
-        y_pre = (y_ini - (math.sin(math.radians(angle)) * speed * time) + 0.5 * gravity * time ** 2)
-        pygame.draw.circle(back_ground_surf,'white',(x_pre,y_pre),5)"""
+
+    for time2 in range(0,30,5):
+        x_pre = (x_ini + math.cos(math.radians(angle)) * speed * time2)
+        y_pre = (y_ini - (math.sin(math.radians(angle)) * speed * time2) + 0.5 * gravity * time2 ** 2)
+        pygame.draw.circle(back_ground_surf,'white',(x_pre,y_pre),5)
+
+
 
     if shoot == True:
         # Calculate location
@@ -93,7 +104,8 @@ while running == True:
             time = 0
 
         time += 1  # Increment time
-
+    if trajectory :
+        back_ground_surf = pygame.transform.scale(pygame.image.load('assets/basketball court.png').convert_alpha(), (950, 600))
     screen.blit(back_ground_surf, back_ground_rect)
     screen.blit(player_surf, player_rect)
     screen.blit(hopper_surf, hopper_rect)
@@ -101,5 +113,7 @@ while running == True:
     screen.blit(ball_surf, (x_val, y_val))
     pygame.draw.rect(screen, 'Blue', player_rect, 5)
     pygame.draw.rect(screen, 'Green', hopper_rect, 5)
+
+    trajectory = False
     clock.tick(60)
     pygame.display.update()
