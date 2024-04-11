b0623aefa94de08ad0781a1db262bfeeb5c5a554
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2024-04-11 23:15:16 +0200

    player animation+ cool features

--- variables.py
+++ variables.py
@@ -17,6 +17,7 @@ back_ground_surf = pygame.transform.scale(pygame.image.load('assets/basketball c
 back_ground_rect = back_ground_surf.get_rect(topleft=(0, 0))
 player_surf = pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player.png').convert_alpha(), (70, 180)), True, False)
 player_rect = player_surf.get_rect(midbottom=(400, 480))
+player_animation_surf = [pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation1.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation2.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation3.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation4.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation5.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation6.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation7.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation8.png').convert_alpha(), (70, 180)), True, False),pygame.transform.flip(pygame.transform.scale(pygame.image.load('assets/Sprite_black_player_animated/Sprite_black_player_animation9.png').convert_alpha(), (70, 180)), True, False)]
 ball_surf = pygame.transform.scale(pygame.image.load('assets/basketball ball.png').convert_alpha(), (50, 50))
 ball_rect = ball_surf.get_rect(center=(400, 480))
 hopper_surf = pygame.transform.scale(pygame.image.load('assets/hooooooop.png').convert_alpha(), (180, 300))
@@ -30,6 +31,8 @@ hitbox_hopper_rect1 = hitbox_hooper_surf.get_rect(topleft = (hopper_rect.topleft
 hitbox_hopper_rect2 = hitbox_hooper_surf.get_rect(topleft = (hopper_rect.topleft[0]+105,hopper_rect.topleft[1]+20))
 hitbox_score_surf = pygame.image.load('assets/hitbox_score.png').convert_alpha()
 hitbox_score_rect = hitbox_score_surf.get_rect(topleft = (hopper_rect.topleft[0]+45,hopper_rect.topleft[1]+60))
+animation_counter = 0
+animation_speed = 100
 # all ball values(parameters):
 bounce_count = 0
 x_ini = player_rect.x+70
@@ -41,6 +44,7 @@ y_pre = y_ini
 mass = 100
 time = 0
 time2 = 0
+time3 = 0
 retention = 0.95
 gravity = 0.5
 shoot = False
